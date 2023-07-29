166a03d8fa84a5a3e5fd0953426ef7941c504b4e
Siggen97 <104718385+Siggen97@users.noreply.github.com>
2023-07-29 13:34:01 +0200

    Update: added more questions to the quiz-app

--- Projects/Quiz-App/js/quizApp.js
+++ Projects/Quiz-App/js/quizApp.js
@@ -85,12 +85,113 @@ function clearStatusClass(element) {
 // Here is my questions using an array
 const questions = [
 	{
-		question: 'When was Arséne Wenger unveiled as the new manager of Arsenal FC?',
-        answer: [
-            {text: '22 September 1996',correct: true},
-            {text: '14 August 1996', correct: false },
-            {text: '1 October 1996', correct: false},
-            {text: '21 January 1997', correct: false}
-        ]
+		question:
+			'When was Arsène Wenger unveiled as the new manager of Arsenal FC?',
+		answer: [
+			{ text: '22 September 1996', correct: true },
+			{ text: '14 August 1996', correct: false },
+			{ text: '1 October 1996', correct: false },
+			{ text: '21 January 1997', correct: false },
+		],
+	},
+	{
+		question:
+			'How many Premier League titles did Arsenal win under Arsène Wenger??',
+		answer: [
+			{ text: '1', correct: false },
+			{ text: '2', correct: false },
+			{ text: '3', correct: true },
+			{ text: '4', correct: false },
+		],
+	},
+	{
+		question:
+			'Arsenal won the FA Cup numerous times. How many FA Cups did they win under Arsène Wenger?',
+		answer: [
+			{ text: '4', correct: false },
+			{ text: '5', correct: false },
+			{ text: '6', correct: true },
+			{ text: '7', correct: false },
+		],
+	},
+	{
+		question:
+			'In which year did Arsène Wenger lead Arsenal to the UEFA Champions League final?',
+		answer: [
+			{ text: '2005', correct: false },
+			{ text: '2006', correct: true },
+			{ text: '2007', correct: false },
+			{ text: '2008', correct: false },
+		],
+	},
+	{
+		question:
+			'How many years did Arsène Wenger spend as the manager of Arsenal?',
+		answer: [
+			{ text: '22 years', correct: true },
+			{ text: '24 years', correct: false },
+			{ text: '21 years', correct: false },
+			{ text: '20 years', correct: false },
+		],
+	},
+	{
+		question:
+			'What is the highest number of points accumulated by Arsenal in a single Premier League season under Arsène Wengers management?',
+		answer: [
+			{ text: '75', correct: false },
+			{ text: '83', correct: false },
+			{ text: '89', correct: true },
+			{ text: '97', correct: false },
+		],
+	},
+	{
+		question:
+			'Who scored the winning goal for Arsenal in the FA Cup final against Manchester United in 2005',
+		answer: [
+			{ text: 'Freddi Ljungberg', correct: false },
+			{ text: 'Robert Pires', correct: false },
+			{ text: 'Sol Campbell', correct: false },
+			{ text: 'Patrik Vieira', correct: true },
+		],
+	},
+	{
+		question:
+			'Over the years Arsène Wenger has brought in a lot of players, but what player was his second signing for the club?',
+		answer: [
+			{ text: 'Kaba Diawara', correct: false },
+			{ text: 'Nicolas Anelka', correct: false },
+			{ text: 'Gilles Grimandi', correct: false },
+			{ text: 'Rémi Garde', correct: true },
+		],
+	},
+	{
+		question:
+			'Before joining Arsenal, Arsène Wenger had managerial stints in which country?',
+		answer: [
+			{ text: 'France', correct: false },
+			{ text: 'Japan', correct: true },
+			{ text: 'Germany', correct: false },
+			{ text: 'South Korea', correct: false },
+		],
+	},
+	{
+		question:
+			'At which stadium did Arsène Wenger get sent off by kicking a plastic bottle?',
+		answer: [
+			{ text: 'Anfield', correct: false },
+			{ text: 'Stamford Bridge', correct: false },
+			{ text: 'Old Trafford', correct: true },
+			{ text: 'Emirates', correct: false },
+		],
+	},
+	{
+		question:
+			'When was Arséne Wenger unveiled as the new manager of Arsenal FC?',
+		answer: [
+			{ text: '22 September 1996', correct: true },
+			{ text: '14 August 1996', correct: false },
+			{ text: '1 October 1996', correct: false },
+			{ text: '21 January 1997', correct: false },
+		],
 	},
 ];
