342e91d418f127c733e3b0ec58ec06433437be25
Siggen97 <104718385+Siggen97@users.noreply.github.com>
2023-07-30 09:53:29 +0200

    Update quizApp.js

--- Projects/Quiz-App/js/quizApp.js
+++ Projects/Quiz-App/js/quizApp.js
@@ -7,79 +7,79 @@ const questionDisplay = document.getElementById('question');
 const answerButtons = document.getElementById('multiple-choice-btn');
 
 // This will be undefined, and be perfect for it's functionality
-let shuffledQuestions, currentQuestionIndex;
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-	currentQuestionIndex++;
-	nextQuestion();
-});
+    currentQuestionIndex++
+    nextQuestion()
+})
 
 function startGame() {
 	console.log('started');
 	startButton.classList.add('hide');
-	shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-	currentQuestionIndex = 0;
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
 	questionContainer.classList.remove('hide');
-	nextQuestion();
+    nextQuestion()
 }
 
 function nextQuestion() {
-	resetState();
-	showQuestion(shuffledQuestions[currentQuestionIndex]);
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question) {
-	questionDisplay.innerText = question.question;
-	question.answer.forEach((answer) => {
-		const button = document.createElement('button');
-		button.innerText = answer.text;
-		button.classList.add('choice-btn');
-		if (answer.correct) {
-			button.dataset.correct = answer.correct;
-		}
-		button.addEventListener('click', selectAnswer);
-		answerButtons.appendChild(button);
-	});
+function showQuestion(question){
+    questionDisplay.innerText = question.question 
+    question.answer.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('choice-btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtons.appendChild(button)  
+    }) 
 }
 
-function resetState() {
-	clearStatusClass(document.body);
-	nextButton.classList.add('hide');
-	while (answerButtons.firstChild) {
-		answerButtons.removeChild(answerButtons.firstChild);
-	}
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
 }
 
 function selectAnswer(e) {
-	const selectedButton = e.target;
-	const correct = selectedButton.dataset.correct;
-	setStatusClass(document.body, correct);
-	Array.from(answerButtons.children).forEach((button) => {
-		setStatusClass(button, button.dataset.correct);
-	});
-	// This one under here is telling us that we have more questions than we are currently on,
-	// and we are not on the last question. We then want our next button to display.
-	if (shuffledQuestions.length > currentQuestionIndex + 1) {
-		nextButton.classList.remove('hide');
-	} else {
-		startButton.innerText = 'Restart';
-		startButton.classList.remove('hide');
-	}
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtons.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    // This one under here is telling us that we have more questions than we are currently on, 
+    // and we are not on the last question. We then want our next button to display.
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
 }
 
-function setStatusClass(element, correct) {
-	clearStatusClass(element);
-	if (correct) {
-		element.classList.add('correct');
-	} else {
-		element.classList.add('wrong');
-	}
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
 }
 
 function clearStatusClass(element) {
-	element.classList.remove('correct');
-	element.classList.remove('wrong');
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 // Here is my questions using an array
